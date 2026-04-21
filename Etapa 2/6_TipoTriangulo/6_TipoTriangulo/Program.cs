0b8b9f78545ba4c593a62f5f7588c7fc2e1eefc7
Matias Huacara <samicamily09@gmail.com>
2026-04-21 10:53:36 -0300

    Subiendo el trabajo numero 6 de la etapa 2

--- Etapa 2/6_TipoTriangulo/6_TipoTriangulo/Program.cs
+++ Etapa 2/6_TipoTriangulo/6_TipoTriangulo/Program.cs
@@ -20,14 +20,29 @@ namespace _6_TipoTriangulo
             lado2 = float.Parse(Console.ReadLine());
             Console.Write("Escriba una medida para un ultimo lado: ");
             lado3 = float.Parse(Console.ReadLine());
-            if (lado1 + lado2 > lado3 || lado2 + lado1 > lado2 || lado2 + lado3 > lado1)
+            if (lado1 + lado2 <= lado3 || lado3 + lado1 <= lado2 || lado2 + lado3 <= lado1)
             {
-                Console.Write("Si puede formar un triangulo");
+                Console.WriteLine("No se puede formar un triangulo");
             }
-            if (lado1==lado2 && lado1==lado3)
+            else if (lado1 + lado2 > lado3 || lado3 + lado1 > lado2 || lado2 + lado3 > lado1)
             {
+                Console.Write("Si se puede formar un triangulo"+",");
 
+                if (lado1 == lado2 && lado1 == lado3 && lado2 == lado3)
+                {
+                    Console.Write("puede formar un triangulo equilatero");
+                }
+                else if (lado1 == lado2 || lado2 == lado3 || lado1 == lado3)
+                {
+                    Console.Write("puede formar un triangulo isosceles");
+                }
+                else
+                {
+                    Console.Write("puede formar un triangulo escaleno");
+                }
             }
+            Console.ReadKey();
+
 
 
         }
